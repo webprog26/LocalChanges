49d03c11a912941f6947a98948a975c26d69f6c6
webprog26 <webprog26@gmail.com>
2017-09-28 13:08:13 +0300

    Button added

--- app/src/main/java/com/developer/webprog26/rebaselocalchanges/MainActivity.java
+++ app/src/main/java/com/developer/webprog26/rebaselocalchanges/MainActivity.java
@@ -2,12 +2,45 @@ package com.developer.webprog26.rebaselocalchanges;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.developer.webprog26.rebaselocalchanges.presenters.MainPresenter;
+import com.developer.webprog26.rebaselocalchanges.presenters.MainPresenterImpl;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @BindView(R.id.btn_click)
+    Button mBtnClick;
+
+    private MainPresenter mainPresenter = new MainPresenterImpl();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        getBtnClick().setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_click:
+                getMainPresenter().reactOnButtonClick();
+                break;
+        }
+    }
+
+    private Button getBtnClick() {
+        return mBtnClick;
+    }
+
+    private MainPresenter getMainPresenter() {
+        return mainPresenter;
     }
 }
